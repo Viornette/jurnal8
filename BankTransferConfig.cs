70a54eb658441eaf62d500786ab5ed45fb60dd0d
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-15 08:22:10 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ BankTransferConfig.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+public class transfer {
+        public int threshold { get; set; }
+        public int low_fee { get; set; }
+        public int high_fee { get; set; }
+}
+public class confirmation {
+        public string en { get; set; }
+        public string id { get; set; }
+}
+        
+public class BankTransferConfig
+{
+    public string lang { get; set; }
+    public transfer transfer{ get; set; }
+    public List<string> methods { get; set; }
+    public confirmation confirmation { get; set; }
+
+    private const string configPath = "bank_transfer_config.json";
+    public BankTransferConfig config;
+
+    public BankTransferConfig LoadFromFile()
+    {
+        String configJsonData = File.ReadAllText(configPath);
+        config = JsonSerializer.Deserialize<BankTransferConfig>(configJsonData); 
+        return config;
+    }
+
+    private void WriteNewConfigFile()
+    {
+        JsonSerializerOptions options = new JsonSerializerOptions()
+        {
+            WriteIndented = true
+        };
+        String jsonString = JsonSerializer.Serialize(config, options);
+        File.WriteAllText(configPath, jsonString);
+    }
+    public void UbahLang()
+    {
+        lang = lang == "en" ? "id" : "en";
+        WriteNewConfigFile();
+    }
+} 
+
--- Program.cs
+++ Program.cs
@@ -1,2 +1,79 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        BankTransferConfig data = new BankTransferConfig();
+        BankTransferConfig config = data.LoadFromFile();
+        config.UbahLang();
+        double biaya;
+
+        if (config.lang == "en")
+        {
+            Console.WriteLine("Please insert the amount of money to transfer: ");
+        }
+        else if (config.lang == "id")
+        {
+             Console.WriteLine("Masukkan jumlah uang yang akan di-transfer: ");
+        }
+        double jumlah = Convert.ToDouble(Console.ReadLine());
+
+        if (jumlah <= config.transfer.threshold)
+        {
+            biaya = Convert.ToDouble(config.transfer.low_fee);
+        }
+        else
+        {
+            biaya = Convert.ToDouble(config.transfer.high_fee);
+        }
+
+        double total = jumlah + biaya;
+
+        if (config.lang == "en")
+        {
+            Console.WriteLine("Transfer fee = " + biaya);
+            Console.WriteLine("Total amount = " + total);
+
+            Console.WriteLine("\nSelect transfer method:");
+        }
+        else if (config.lang == "id")
+        {
+            Console.WriteLine("Biaya transfer =  " + biaya);
+            Console.WriteLine("Total biaya = " + total);
+
+            Console.WriteLine("\nPilih metode transfer:");
+        }
+
+        for (int i = 0; i < config.methods.Count; i++)
+        {
+            Console.WriteLine($" {config.methods[i]}");
+        }
+        string metode = Console.ReadLine();
+
+        if (config.lang == "en")
+        {
+            Console.WriteLine($"Please type {config.confirmation.en} to confirm the transaction:");
+            string confirm = Console.ReadLine();
+            if (confirm == config.confirmation.en)
+            {
+                Console.WriteLine("The transfer is completed");
+            }
+            else
+            {
+                Console.WriteLine("Transfer is cancelled");
+            }
+        }
+        else if (config.lang == "id")
+        {
+            Console.WriteLine($"Ketik {config.confirmation.id}  untuk mengkonfirmasi transaksi:");
+            string confirm = Console.ReadLine();
+            if (confirm == config.confirmation.id)
+            {
+                Console.WriteLine("Proses transfer berhasil");
+            }
+            else
+            {
+                Console.WriteLine("Transfer dibatalkan");
+            }
+        }
+    }
+}
